5ee20757a5e3807acac2572344ec72ff8ec7983b
vicpaton <victor.paton@outlook.com>
2024-04-10 14:43:23 +0200

    added pagerank and ppr as diffusion-like

--- networkcommons/methods.py
+++ networkcommons/methods.py
@@ -138,6 +138,7 @@ def all_paths(network, source_df, target_df, depth_cutoff=None, verbose=False, n
 
     return subnetwork, all_paths_res
 
+
 def compute_all_paths(network, source, targets, cutoff):
     """
     Compute all paths between source and targets.
@@ -157,4 +158,91 @@ def compute_all_paths(network, source, targets, cutoff):
         paths_for_source.extend(paths)
 
     return paths_for_source
+
+
+def add_pagerank_scores(network, source_df, target_df, alpha=0.85, max_iter=100, tol=1.0e-6, nstart=None, weight='weight', personalize_for=None):
+    """
+    Add PageRank scores to the nodes of the network.
+
+    Args:
+        network (nx.Graph): The network.
+        source_df (pd.DataFrame): A pandas DataFrame containing the sources.
+        target_df (pd.DataFrame): A pandas DataFrame containing the targets. Must contain three columns: source, target and sign
+        percentage (int): Percentage of nodes to keep.
+        alpha (float): Damping factor for the PageRank algorithm.
+        max_iter (int): Maximum number of iterations.
+        tol (float): Tolerance to determine convergence.
+        nstart (dict): Starting value of PageRank iteration for all nodes.
+        weight (str): Edge data key to use as weight.
+        personalize_for (str): Personalize the PageRank by setting initial probabilities for either sources or targets.
+
+    Returns:
+        tuple: Contains nodes above threshold from sources, nodes above threshold from targets, and overlapping nodes.
+    """
+    sources = set(source_df['source'].values)
+    targets = set(target_df['target'].values)
+
+    if personalize_for == "source":
+        personalized_prob = {n: 1/len(sources) for n in sources}
+    elif personalize_for == "target":
+        personalized_prob = {n: 1/len(targets) for n in targets}
+        network = network.reverse()
+    else:
+        personalized_prob = None
+
+    pagerank = nx.pagerank(network, 
+                           alpha=alpha, 
+                           max_iter=max_iter, 
+                           personalization=personalized_prob, 
+                           tol=tol, nstart=nstart, 
+                           weight=weight, 
+                           dangling=personalized_prob)
+    
+    if personalize_for == "target":
+        network = network.reverse()
+
+    for node, pr_value in pagerank.items():
+        if personalize_for == "target":
+            attribute_name = 'pagerank_from_targets'
+        elif personalize_for == "source":
+            attribute_name = 'pagerank_from_sources'
+        elif personalize_for is None:
+            attribute_name = 'pagerank'
+        network.nodes[node][attribute_name] = pr_value
+
+    return network
+
+
+def compute_ppr_overlap(network, percentage=20):
+    """
+    Compute the overlap of nodes that exceed the personalized PageRank percentage threshold from sources and targets.
+
+    Args:
+        network (nx.Graph): The network.
+        percentage (int): Percentage of top nodes to keep.
+
+    Returns:
+        tuple: Contains nodes above threshold from sources, nodes above threshold from targets, and overlapping nodes.
+    """
+    # Sorting nodes by PageRank score from sources and targets
+    try:
+        sorted_nodes_sources = sorted(network.nodes(data=True), key=lambda x: x[1].get('pagerank_from_sources'), reverse=True)
+        sorted_nodes_targets = sorted(network.nodes(data=True), key=lambda x: x[1].get('pagerank_from_targets'), reverse=True)
+    except KeyError:
+        raise KeyError("Please run the add_pagerank_scores method first with personalization options.")
+
+    # Calculating the number of nodes to keep
+    num_nodes_to_keep_sources = int(len(sorted_nodes_sources) * (percentage / 100))
+    num_nodes_to_keep_targets = int(len(sorted_nodes_targets) * (percentage / 100))
+
+    # Selecting the top nodes
+    nodes_above_threshold_from_sources = {node[0] for node in sorted_nodes_sources[:num_nodes_to_keep_sources]}
+    nodes_above_threshold_from_targets = {node[0] for node in sorted_nodes_targets[:num_nodes_to_keep_targets]}
+
+    overlap = nodes_above_threshold_from_sources.intersection(nodes_above_threshold_from_targets)
+    nodes_to_include = nodes_above_threshold_from_sources.union(nodes_above_threshold_from_targets)
+
+    subnetwork = network.subgraph(nodes_to_include)
+
+    return subnetwork
 
