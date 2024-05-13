9c4d0fa6862df4a49c81af8067f5bbcb392a1bb0
vicpaton <victor.paton@outlook.com>
2024-05-13 14:26:42 +0200

    removed int conversion for version

--- networkcommons/__init__.py
+++ networkcommons/__init__.py
@@ -1,5 +1,5 @@
 __version__ = '0.0.dev1'  # noqa: F401 F403
-__version_info__ = tuple([int(num) for num in __version__.split('.')])  # noqa: E501 F401 F403
+__version_info__ = tuple([num for num in __version__.split('.')])  # noqa: E501 F401 F403
 
 from .utils import *  # noqa: F401 F403
 from .methods import *  # noqa: F401 F403
