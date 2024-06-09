fabade52ff94aa37536a517aa686c987ff6f6360
deeenes <turei.denes@gmail.com>
2024-06-10 00:31:32 +0200

    moved `evaluation` → `_eval._graph`

--- networkcommons/_eval/_graph.py
+++ networkcommons/_eval/_graph.py
@@ -1,6 +1,35 @@
+#!/usr/bin/env python
+
+#
+# This file is part of the `networkcommons` Python module
+#
+# Copyright 2024
+# Heidelberg University Hospital
+#
+# File author(s): Saez Lab (omnipathdb@gmail.com)
+#
+# Distributed under the GPLv3 license
+# See the file `LICENSE` or read a copy at
+# https://www.gnu.org/licenses/gpl-3.0.txt
+#
+
+"""
+Basic graph metrics.
+"""
+
 import pandas as pd
 import networkx as nx
 import numpy as np
+
+__all__ = [
+    'get_number_nodes',
+    'get_number_edges',
+    'get_mean_degree',
+    'get_mean_betweenness',
+    'get_mean_closeness',
+    'get_connected_targets',
+    'get_graph_metrics',
+]
 
 
 def get_number_nodes(network):
---
+++ tests/test_eval_graph.py
@@ -0,0 +1,88 @@
+import pytest
+
+import networkx as nx
+import pandas as pd
+
+import networkcommons._eval._graph as _graph
+
+
+@pytest.fixture
+def network():
+
+    network = nx.DiGraph()
+    network.add_edge('A', 'B', weight=1)
+    network.add_edge('B', 'C', weight=2)
+    network.add_edge('C', 'D', weight=3)
+    network.add_edge('C', 'E', weight=3)
+    network.add_edge('A', 'D', weight=6)
+    network.add_edge('A', 'E', weight=4)
+    network.add_edge('E', 'F', weight=5)
+
+    return network
+
+
+def test_get_number_nodes():
+
+    assert _graph.get_number_nodes(nx.Graph()) == 0
+    assert _graph.get_number_nodes(nx.Graph([(1, 2)])) == 2
+    assert _graph.get_number_nodes(nx.Graph([(1, 2), (2, 3)])) == 3
+
+
+def test_get_number_edges():
+
+    assert _graph.get_number_edges(nx.Graph()) == 0
+    assert _graph.get_number_edges(nx.Graph([(1, 2)])) == 1
+    assert _graph.get_number_edges(nx.Graph([(1, 2), (2, 3)])) == 2
+
+
+def test_get_mean_degree(network):
+
+    assert _graph.get_mean_degree(network) == 7/3
+
+
+def test_get_mean_betweenness(network):
+
+    assert _graph.get_mean_betweenness(network) == 0.05833333333333334
+
+
+def test_get_mean_closeness(network):
+
+    assert _graph.get_mean_closeness(network) == 0.29444444444444445
+
+
+def test_get_connected_targets(network):
+
+    target_dict = {'D': 1, 'F': 1, 'W': 1}
+
+    assert _graph.get_connected_targets(network, target_dict) == 2
+    assert (
+        _graph.get_connected_targets(network, target_dict, percent=True) ==
+        2 / 3 * 100
+    )
+
+
+def test_get_recovered_offtargets(network):
+
+    offtargets = ['B', 'D', 'W']
+
+    assert _graph.get_recovered_offtargets(network, offtargets) == 2
+    assert (
+        _graph.get_recovered_offtargets(network, offtargets, percent=True) ==
+        2 / 3 * 100
+    )# noqa: E501
+
+
+def test_get_graph_metrics(network):
+
+    target_dict = {'D': 1, 'F': 1, 'W': 1}
+
+    metrics = pd.DataFrame({
+        'Number of nodes': 6,
+        'Number of edges': 7,
+        'Mean degree': 7/3,
+        'Mean betweenness': 0.05833333333333334,
+        'Mean closeness': 0.29444444444444445,
+        'Connected targets': 2
+    }, index=[0])
+
+    assert _graph.get_graph_metrics(network, target_dict).equals(metrics)
