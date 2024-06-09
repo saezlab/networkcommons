9efa05c373aebdf4cb9f42b7ac3765c53c0e570e
deeenes <turei.denes@gmail.com>
2024-06-09 16:12:44 +0200

    `test_vis`: use `pytest` instead of `unittest` + minor refactor

--- tests/test_visualisation.py
+++ tests/test_visualisation.py
@@ -1,41 +1,43 @@
-import unittest
+import pytest
 import networkx as nx
-from networkcommons.visualize_network import {
-    get_styles, 
-    merge_styles, 
-    set_style_attributes, 
-    visualize_network_default, 
-    visualize_network_sign_consistent, 
-    visualize_network
-}
 
-class TestVisualizeNetwork(unittest.TestCase):
+import networkcommons.visualization as _vis
 
-    def setUp(self):
+
+class TestVisualizeNetwork:
+
+
+    @pytest.fixture
+    def minigraph(self) -> tuple[nx.DiGraph, dict, dict]:
         # Create a sample graph for testing
-        self.G = nx.DiGraph()
-        self.G.add_node("A")
-        self.G.add_node("B")
-        self.G.add_node("C")
-        self.G.add_edge("A", "B")
-        self.G.add_edge("B", "C")
-        self.G.add_edge("C", "A")
+        G = nx.DiGraph()
+        G.add_node('A')
+        G.add_node('B')
+        G.add_node('C')
+        G.add_edge('A', 'B')
+        G.add_edge('B', 'C')
+        G.add_edge('C', 'A')
 
-        # Define source and target dictionaries
-        self.source_dict = {"A": 1, "B": -1}
-        self.target_dict = {"C": {"value": 1}}
+        source_dict = {'A': 1, 'B': -1}
+        target_dict = {'C': {'value': 1}}
+
+        return G, source_dict, target_dict
+
 
     def test_get_styles(self):
         # Test that the styles dictionary contains the expected keys
-        styles = get_styles()
-        self.assertIn('default', styles)
-        self.assertIn('sign_consistent', styles)
-        self.assertIn('nodes', styles['default'])
-        self.assertIn('edges', styles['default'])
+        styles = _vis.get_styles()
+
+        assert 'default' in styles
+        assert 'sign_consistent' in styles
+        assert 'nodes' in styles['default']
+        assert 'edges' in styles['default']
+
 
     def test_merge_styles(self):
+
         # Test merging custom styles with default styles
-        default_style = get_styles()['default']
+        default_style = _vis.get_styles()['default']
         custom_style = {
             'nodes': {
                 'sources': {
@@ -43,34 +45,48 @@ class TestVisualizeNetwork(unittest.TestCase):
                 }
             }
         }
-        merged_style = merge_styles(default_style, custom_style)
-        self.assertEqual(merged_style['nodes']['sources']['color'], 'red')
-        self.assertEqual(merged_style['nodes']['sources']['shape'], 'circle')  # Default value
+        merged_style = _vis.merge_styles(default_style, custom_style)
 
-    def test_set_style_attributes(self):
+        assert merged_style['nodes']['sources']['color'] == 'red'
+        assert merged_style['nodes']['sources']['shape'] == 'circle'  # Default value
+
+
+    def test_set_style_attributes(self, minigraph):
         # Test setting style attributes on a node
-        default_style = get_styles()['default']
-        A = nx.nx_agraph.to_agraph(self.G)
-        node = A.get_node("A")
-        set_style_attributes(node, default_style['nodes']['sources'])
-        self.assertEqual(node.attr['shape'], 'circle')
-        self.assertEqual(node.attr['color'], 'steelblue')
+        default_style = _vis.get_styles()['default']
+        A = nx.nx_agraph.to_agraph(minigraph[0])
+        node = A.get_node('A')
+        _vis.set_style_attributes(node, default_style['nodes']['sources'])
 
-    def test_visualize_network_default(self):
+        assert node.attr['shape'] == 'circle'
+        assert node.attr['color'] == 'steelblue'
+
+
+    def test_visualize_network_default(self, minigraph):
+
+        G, source_dict, target_dict = minigraph
         # Test visualizing the network with default style
-        A = visualize_network_default(self.G, self.source_dict, self.target_dict)
-        self.assertTrue(A)  # Check that the function returns an AGraph object
+        A = _vis.visualize_network_default(G, source_dict, target_dict)
 
-    def test_visualize_network_sign_consistent(self):
+        assert A  # Check that the function returns an AGraph object
+
+
+    def test_visualize_network_sign_consistent(self, minigraph):
+
+        G, source_dict, target_dict = minigraph
         # Add interactions to edges for sign consistency
-        self.G["A"]["B"]["interaction"] = 1
-        self.G["B"]["C"]["interaction"] = -1
-        self.G["C"]["A"]["interaction"] = 1
+        G['A']['B']['interaction'] = 1
+        G['B']['C']['interaction'] = -1
+        G['C']['A']['interaction'] = 1
         # Test visualizing the network considering sign consistency
-        A = visualize_network_sign_consistent(self.G, self.source_dict, self.target_dict)
-        self.assertTrue(A)  # Check that the function returns an AGraph object
+        A = _vis.visualize_network_sign_consistent(G, source_dict, target_dict)
 
-    def test_visualize_network_with_custom_style(self):
+        assert A  # Check that the function returns an AGraph object
+
+
+    def test_visualize_network_with_custom_style(self, minigraph):
+
+        G, source_dict, target_dict = minigraph
         # Define a custom style
         custom_style = {
             'nodes': {
@@ -84,8 +100,12 @@ class TestVisualizeNetwork(unittest.TestCase):
             }
         }
         # Test visualizing the network with a custom style
-        A = visualize_network(self.G, self.source_dict, self.target_dict, network_type='default', custom_style=custom_style)
-        self.assertTrue(A)  # Check that the function returns an AGraph object
+        A = _vis.visualize_network(
+            G,
+            source_dict,
+            target_dict,
+            network_type = 'default',
+            custom_style = custom_style,
+        )
 
-# if __name__ == '__main__':
-#     unittest.main()
+        assert A  # Check that the function returns an AGraph object
