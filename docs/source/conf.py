241de7aecb6dc2f7bcacada2bad63d738bdb6992
vicpaton <victor.paton@outlook.com>
2024-06-19 20:25:01 +0300

    changed dir

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from datetime import datetime
-sys.path.insert(0, os.path.abspath('../../networkcommons'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information
 
