56404e20c4a920d2947e73c6695e18cf24c6824b
Olga Ivanova <ivanovaos.09@gmail.com>
2024-06-25 17:38:50 +0200

    adding first setup for network obj

--- networkcommons/network/_network.py
+++ networkcommons/network/_network.py
@@ -1,6 +1,15 @@
+from __future__ import annotations
+
+from networkcommons._noi._noi import Noi
 
 class Network:
 
-    def __init__(self):
+    def __init__(self,
+                 noi: Noi | list[str] | list[list[str]] | dict[str, list[str]],
+                 universe: str | None = "omnipath"):
+        self.universe = universe
+        self.noi = noi
 
-        pass
+
+
+
