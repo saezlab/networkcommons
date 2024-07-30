a6ed1ab120d07f18dc1978ef9c143e0069c13520
vicpaton <victor.paton@outlook.com>
2024-07-30 16:23:29 +0200

    added new handler for cosmos pkn

--- networkcommons/data/network/_moon.py
+++ networkcommons/data/network/_moon.py
@@ -17,17 +17,49 @@
 Prior knowledge network used by MOON.
 """
 
-__all__ = ['build_moon_regulons']
+__all__ = ['build_moon_regulons', 'get_cosmos_pkn']
 
 import lazy_import
 import numpy as np
 import pandas as pd
 
-dc = lazy_import.lazy_module('decoupler')
-
 from networkcommons import _utils
 from . import _omnipath
 from . import _liana
+
+import os
+import urllib
+from networkcommons import _conf
+from networkcommons.data.omics import _common
+
+dc = lazy_import.lazy_module('decoupler')
+
+
+def get_cosmos_pkn(update: bool = False):
+    """
+    Retrieves the metabolic network used in COSMOS from the server
+
+    Returns:
+        network (pandas.DataFrame): metabolic network with
+        source, target, and sign columns.
+    """
+    path = os.path.join(_conf.get('pickle_dir'), 'metapkn.pickle')
+
+    if update or not os.path.exists(path):
+
+        baseurl = urllib.parse.urljoin(_common._baseurl(), 'prior_knowledge')
+
+        file_legend = pd.read_csv(baseurl + '/meta_network.sif', sep='\t')
+
+        file_legend.to_pickle(path)
+
+    else:
+
+        file_legend = pd.read_pickle(path)
+
+    return file_legend
+
+
 
 
 def build_moon_regulons(include_liana=False):
