ff8eaba8ec6123cfd9d709a899fb19cd70713ba1
deeenes <turei.denes@gmail.com>
2024-09-05 15:06:40 +0200

    `network.formats.igraph` (WIP)

---
+++ networkcommons/network/_formats/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+#
+# This file is part of the `networkcommons` Python module
+#
+# Copyright 2024
+# Heidelberg University Hospital
+#
+# File author(s): Saez Lab (omnipathdb@gmail.com)
+#
+# Distributed under the GPLv3 license
+# See the file `LICENSE` or read a copy at
+# https://www.gnu.org/licenses/gpl-3.0.txt
+#
+
+"""
+Conversion to and from different network formats.
+"""
